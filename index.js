64485f9c930777589351c96973b4c425fa2e6394
henryshop73 <henrymoranfreelance@gmail.com>
2022-05-26 00:00:18 -0500

    Agregando función calcular

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 
-function suma(firstNumber, secondNumber) {
+/* function suma(firstNumber, secondNumber) {
     let result = firstNumber + secondNumber
     console.log(result)
 }
 
 suma(3, 2)
-suma(5, 7)
+suma(5, 7) */
 
+function calcular (firstNumber, secondNumber, operation) {
+    let result
+    switch (operation) {
+        case "suma":
+           result = firstNumber + secondNumber
+            break;
+        case "multiplicar":
+            result = firstNumber * secondNumber
+             break;
+        case "dividir":
+            result = firstNumber / secondNumber
+            break
+    
+        default:
+           console.log("Operación no valida")
+            break; 
+        
+    }  
+    console.log(result)
+
+}
+
+calcular(2, 3, "suma")
+calcular(3, 5, "multiplicar")
+calcular (10, 2, "dividir")
+calcular(2, 6, "hola")
