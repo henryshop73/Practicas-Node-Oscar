20a858e6d4fc5ccc54e96c178ff36b160bd8d57a
henryshop73 <henrymoranfreelance@gmail.com>
2022-05-26 22:59:05 -0500

    Estructuras y tipos de datos

---
+++ estructura_de_datos.js
@@ -0,0 +1,34 @@
+/*class Persona {
+    nombre = "henry"
+    ciudad = "Lima"
+    edad = 49
+
+    saludar() {
+        return "saludo"
+    }
+}
+let persona1 = new Persona()
+console.log (persona1.ciudad)
+console.log (persona1.nombre)
+console.log (persona1.edad)
+console.log(persona1.saludar())*/
+
+class persona {
+    nombre
+    edad
+    ciudad
+
+    constructor(valorNombre, valorEdad, nombreCiudad) {
+        this.nombre = valorNombre
+        this.edad = valorEdad
+        this.ciudad = nombreCiudad
+    } 
+}
+let hombre = new persona("Henry", 49, "Lima")
+let hombre2 = new persona("Oscar", 32, "Bogotá")
+console.log(hombre.nombre)
+console.log(hombre.edad)
+console.log(hombre.ciudad)
+console.log(hombre2.ciudad)
+console.log(typeof(hombre))
+
---
+++ tipo_de_datos.js
@@ -0,0 +1,13 @@
+let a = "Saludos"
+
+console.log(typeof("Hola"))
+console.log(typeof(10))
+console.log(typeof(10.5))
+console.log(typeof(true))
+console.log(typeof(a))
+console.log(typeof(null))
+
+function soyUnaFuncion() {
+    return 1
+}
+console.log(typeof(soyUnaFuncion))
