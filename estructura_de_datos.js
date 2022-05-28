e53b8b273dc8a088b9b467dc92503e7d65667aad
henryshop73 <henrymoranfreelance@gmail.com>
2022-05-27 20:14:33 -0500

    Agregando calculadora utilizando clases

--- estructura_de_datos.js
+++ estructura_de_datos.js
@@ -11,7 +11,7 @@ let persona1 = new Persona()
 console.log (persona1.ciudad)
 console.log (persona1.nombre)
 console.log (persona1.edad)
-console.log(persona1.saludar())*/
+console.log(persona1.saludar())
 
 class persona {
     nombre
@@ -40,4 +40,38 @@ console.log(hombre.saludar())
 
 
 console.log()
-hombre.saludar()
+hombre.saludar() */
+
+class Calculadora {
+    constructor(firstNumber, secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    suma() {
+        return this.firstNumber + this.secondNumber;
+    }
+
+    resta() {
+        return this.firstNumber - this.secondNumber;
+    }
+
+    multiplicacion() {
+        return this.firstNumber * this.secondNumber;
+    }
+
+    division() {
+        return this.firstNumber / this.secondNumber;
+    }
+}
+
+let calcSuma = new Calculadora(8, 5);
+let calcResta = new Calculadora(7, 3)
+let calcMultiplicar = new Calculadora(5, 5)
+let calcDivision = new Calculadora(30, 10)
+console.log(calcSuma.suma());
+console.log(calcResta.resta());
+console.log(calcMultiplicar.multiplicacion());
+console.log(calcDivision.division());
+
+
